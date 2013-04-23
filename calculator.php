5dc4a97a78a650227f946e18d78f3cc067eaf1a5
ahsprathyusha <prathyusha.ahs@gmail.com>
2013-04-23 14:53:32 +0530

    updated

--- calculator.php
+++ calculator.php
@@ -6,5 +6,5 @@ echo "Calc Application";
 
 <br>
 
-The sum of two numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is <br>
+The sum of 2 numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is <br>
 <?php echo $_GET["num1"]+$_GET["num2"]; ?> !
